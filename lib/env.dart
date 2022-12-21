472e2f63d189b11a8c58b800113a428ae9d982b9
youknowme <74351253+ys1113457623@users.noreply.github.com>
2022-12-21 21:36:05 +0530

    removed env

--- lib/env.dart
+++
@@ -1,4 +0,0 @@
-class Secrets {
-  String accessKeyId = "W5kAArx54lZwNR4fHSqUtw";
-  String accessKeySecret = "9fXpWyoo0ffvEUwtTp1-pFZU1L3V_XB66wnwPb_hmgkRv4t2QXxUGi7-dRonTJAANu8d1muZbdMEo34suX7VtQ";
-}
